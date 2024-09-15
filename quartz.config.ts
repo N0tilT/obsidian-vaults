a6f148c601088084ec8a7c1cd2724d071192e474
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-09-15 12:25:37 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Lectures",
+    pageTitle: "Лекции ПИН2106",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
